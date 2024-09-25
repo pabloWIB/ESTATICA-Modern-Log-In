1cdb5bc553f952487240f37aba0cecdea4f14180
Pablo <pablonietoperez369@gmail.com>
2024-09-25 00:13:17 -0500

    Cambios

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+       
+      </header>
+      <nav>
+        
+      </nav>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MyComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyComponent = () => {
+    return <h1>¡Hola, mundo!</h1>;
+}
+
+export default MyComponent;
