958054f4cbf80923b93f82bb2d8c6c8d3354797b
Pablo <pablonietoperez369@gmail.com>
2024-09-25 13:43:00 -0500

    Cambios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React, { useEffect } from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './STYLES/App.css';
 import Spline from '@splinetool/react-spline';
 
+import eyeIcon from './IMG/visibility_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg'; 
+import eyeOffIcon from './IMG/visibility_off_24dp_C7D2D6_FILL0_wght400_GRAD0_opsz24.svg'; 
+import google from './IMG/flat-color-icons_google.svg'; 
+import facebook from './IMG/bi_facebook.svg'; 
+import github from './IMG/akar-icons_github-fill.svg'; 
+
 function App() {
+    const [showPassword, setShowPassword] = useState(false);
+
     useEffect(() => {
         const handleKeyDown = (event) => {
             if (event.ctrlKey && (event.key === '+' || event.key === '-' || event.key === '0')) {
@@ -18,36 +26,68 @@ function App() {
         };
     }, []);
 
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
     return (
         <div>
             <header>
                 <Spline scene="https://prod.spline.design/BedHsbQnoGzxGCVf/scene.splinecode" />
             </header>
             <nav>
-                <h1>Welcome to WIB</h1>
-                <form>
+                <div>
+                    <h1>Welcome to WIB</h1>
                     <h2>Login</h2>
-                    <label htmlFor='email'>Email</label>
-                    <input id='email' type='email' placeholder='username@gmail.com' />
-                    <label htmlFor='password'>Password</label>
-                    <input id='password' type='password' placeholder='Password' />
-                    <a href="#">Forgot Password?</a>
-                    <button type="submit">Sign in</button>
-
-                    <p>Or continue with</p>
+                </div>
+                <form>
                     <div>
-                        <div>
-                            <img alt="Option 1" />
-                        </div>
-                        <div>
-                            <img alt="Option 2" />
-                        </div>
-                        <div>
-                            <img alt="Option 3" />
-                        </div>
+                        <label htmlFor='email'>
+                            <span>Email</span>
+                            <div>
+                                <input id='email' type='email' placeholder='username@gmail.com' />
+                            </div>
+                        </label>
+                        <label htmlFor='password'>
+                            <span>Password</span>
+                            <div>
+                                <input 
+                                    id='password' 
+                                    type={showPassword ? 'text' : 'password'} 
+                                    placeholder='Password' 
+                                />
+                                <div className='hiddenIcons' id='hiddenIcons' onClick={togglePasswordVisibility}>
+                                    <div>
+                                        <img 
+                                            src={showPassword ? eyeOffIcon : eyeIcon} 
+                                            alt={showPassword ? 'Ocultar contraseña' : 'Mostrar contraseña'} 
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </label>
                     </div>
-                    <p>Don't have an account yet? <span>Register for free</span></p>
+                    <div>
+                        <div id='forgotPassword' className='forgotPassword'>
+                            {/* Aquí puedes añadir contenido si es necesario */}
+                        </div>
+                        <a href="#">Forgot Password?</a>
+                    </div>
+                    <button type="submit">Sign in</button>
                 </form>
+                <p>Or continue with</p>
+                <div>
+                    <a href=''>
+                        <img src={google} alt="Option 1" />
+                    </a>
+                    <a href=''>
+                        <img src={facebook} alt="Option 2" />
+                    </a>
+                    <a href=''>
+                        <img src={github} alt="Option 3" />
+                    </a>
+                </div>
+                <p>Don't have an account yet? <span>Register for free</span></p>
             </nav>
         </div>
     );
